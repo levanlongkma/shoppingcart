71ceb4c663fdf7d710b7480e7b5c0b1cdddbecba
Tuong <vantuongno1@gmail.com>
2021-12-11 14:42:58 +0700

    [Add] ghep layout shoping

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Shoping\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::group(['prefix' => 'shoping.'], function() {
+    Route::get('/home/index', [HomeController::class, 'home'])->name('home');
+// });
