611448186cfb2475af9fe037e8f59440f3f10568
Tuong <vantuongno1@gmail.com>
2021-12-16 23:35:30 +0700

    [update] da sua ok

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    public function SetPasswordAttribute($value)
+    public function setPasswordAttribute($value)
     {
         $this->attributes['password']=Hash::make($value);
     }
