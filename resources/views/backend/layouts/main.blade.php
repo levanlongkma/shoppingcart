c2de2d66bd87ef3db3a3c54b5557575f2afaa610
doidangnam <94689458+doidangnam@users.noreply.github.com>
2021-12-18 15:32:10 +0700

    remove tailwind

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -15,7 +15,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
     <link rel="stylesheet" href="{{ asset('backend/assets/css/cs-skin-elastic.css') }}">
     <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/assets/css/responsive.css') }}">
 
     <style>
     #weatherWidget .currentDesc {
