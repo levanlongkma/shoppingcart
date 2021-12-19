b776e471d644ebb577f9f1ab503c843d13556ba5
doidangnam <94689458+doidangnam@users.noreply.github.com>
2021-12-19 12:40:46 +0700

    Final Admin Theme

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -1,55 +1,86 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Login Page</title>
-   <!--Made with love by Mutiullah Samim -->
-   
-	<!--Bootsrap 4 CDN-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
-    <!--Fontawesome CDN-->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
-	<!--Custom styles-->
-	<link rel="stylesheet" type="text/css" href=" {{ asset('backend/assets/css/login-form-style.css') }} ">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="apple-touch-icon" sizes="76x76" href=" {{asset('backend/assets/img/apple-icon.png')}} ">
+	<link rel="icon" type="image/png" href="{{ asset('backend/assets/img/favicon.png ')}}">
+	<title>
+		Admin Login
+	</title>
+	<!--     Fonts and icons     -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+	<!-- Nucleo Icons -->
+	<link href="{{ asset('backend/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+	<link href="{{ asset('backend/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+	<!-- Font Awesome Icons -->
+	<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+	<link href="{{ asset('backend/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+	<!-- CSS Files -->
+	<link id="pagestyle" href="{{ asset('backend/assets/css/soft-ui-dashboard.css?v=1.0.3') }}" rel="stylesheet" />
 </head>
-<body>
-<div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Login</h3>
-			</div>
-			<div class="card-body">
-				<form>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input type="text" class="form-control" placeholder="email">
-						
+
+<body class="">
+<main class="main-content  mt-0">
+<section>
+	<div class="page-header min-vh-75">
+	<div class="container">
+		<div class="row">
+		<div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+			<div class="card card-plain mt-8">
+				<div class="card-header pb-0 text-left bg-transparent">
+					<h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
+					<p class="mb-0">Enter your email and password to sign in</p>
+				</div>
+				<div class="card-body">
+					<form role="form">
+					<label>Email</label>
+					<div class="mb-3">
+						<input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
 					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input type="password" class="form-control" placeholder="password">
+					<label>Password</label>
+					<div class="mb-3">
+						<input type="email" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
 					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
+					<div class="form-check form-switch">
+						<input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+						<label class="form-check-label" for="rememberMe">Remember me</label>
 					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
+					<div class="text-center">
+						<button type="button" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
 					</div>
-				</form>
+					</form>
+				</div>
 			</div>
 		</div>
+		<div class="col-md-6">
+			<div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+			<div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('{{asset('backend/assets/img/curved-images/curved6.jpg')}}')"></div>
+			</div>
+		</div>
+		</div>
 	</div>
-</div>
+	</div>
+</section>
+</main>
+<!--   Core JS Files   -->
+<script src="{{ asset('backend/assets/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ asset('backend/assets/css/nucleo-svg.css') }}"></script>
+<script src="{{ asset('backend/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+<script>
+	var win = navigator.platform.indexOf('Win') > -1;
+	if (win && document.querySelector('#sidenav-scrollbar')) {
+	var options = {
+		damping: '0.5'
+	}
+	Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+	}
+</script>
+<!-- Github buttons -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+<script src="{{ asset('backend/assets/js/soft-ui-dashboard.min.js?v=1.0.3') }}"></script>
 </body>
+
 </html>
