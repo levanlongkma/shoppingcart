9af01d34af00f3feaf189b8d0e31413aa4553b6b
doidangnam <94689458+doidangnam@users.noreply.github.com>
2021-12-25 22:20:03 +0700

    Removing files

--- resources/views/backend/successMessage.blade.php
+++
@@ -1,8 +0,0 @@
-@if (request()->session()->has('success'))
-<div x-data="{ show: true }"
-    x-init="setTimeout(() => show = false, 4000)"
-    x-show="show"
-    class="bg-gradient-warning text-center text-white py-2 px-4 h2">
-    <p>{{ request()->session()->get('success') }}</p>
-</div>
-@endif
