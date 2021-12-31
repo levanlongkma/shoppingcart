a434d5d08439e7918b28751040d2900f512c9a8e
doidangnam <94689458+doidangnam@users.noreply.github.com>
2021-12-31 09:46:33 +0700

    [WIP]

---
+++ resources/views/backend/categories/delete.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Please confirm!</h5>
+            </div>
+            <div class="modal-body">
+            <input type="hidden" name="deleteId">
+            <span id="message_delete"></span>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                <button type="button" class="btn btn-success" id="buttonConfirmDelete">Yes</button>
+                </div>
+        </div>
+    </div>
+</div>
