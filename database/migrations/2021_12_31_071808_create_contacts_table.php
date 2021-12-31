01100b86bd0f490cd7c344a6dc5dd13fe43d65af
doidangnam <94689458+doidangnam@users.noreply.github.com>
2021-12-31 14:28:08 +0700

    [Update] create_contacts_table

---
+++ database/migrations/2021_12_31_071808_create_contacts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->tinyText('name')->nullable();
+            $table->string('phonenumber')->nullable();
+            $table->string('fax')->nullable();
+            $table->text('address')->nullable();
+            $table->string('email')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
